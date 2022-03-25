ece0552b824cb017fd579013e93e0e4fa31b1bf8
Alberto Arcagni <alberto.arcagni@uniroma1.it>
2022-03-25 13:20:02 +0000

    version 1.2.6

--- R/rmProfiles.wprof.R
+++ R/rmProfiles.wprof.R
@@ -1,8 +1,8 @@
 rmProfiles.wprof <-
 function(y, v, ...) {
     if (is.logical(v)) v <- which(v)
-    if (is.character(v)) v <- which(rownames(prof$profiles) %in% v)
-    prof$profiles <- prof$profiles[-v,]
-    prof$freq <- prof$freq[-v]
-    return(prof)
+    if (is.character(v)) v <- which(rownames(y$profiles) %in% v)
+    y$profiles <- y$profiles[-v,]
+    y$freq <- y$freq[-v]
+    return(y)
 }

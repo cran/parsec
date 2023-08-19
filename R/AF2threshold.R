a92f381f2df1aa8c058c26eb6da052910bc64756
Alberto Arcagni <alberto.arcagni@uniroma1.it>
2023-08-19 13:40:02 +0000

    version 1.2.7

--- R/AF2threshold.R
+++ R/AF2threshold.R
@@ -1,10 +1,15 @@
 AF2threshold <-
 function(mpi, prof, zeta = NULL) {
-    if (class(mpi)!="ophi") stop("mpi is not an object of class 'ophi'")
-    if (class(prof)!="wprof") stop("prof is not an object of class 'wprof'")
+#     if (class(mpi)!="ophi")
+      if (!is(mpi, "ophi"))
+        stop("mpi is not an object of class 'ophi'")
+#     if (class(prof)!="wprof")
+      if (!is(prof, "wprof"))
+        stop("prof is not an object of class 'wprof'")
     if (!is.null(zeta)) {
-        if (class(zeta)!="incidence")
-            stop("zeta is not an object of class 'incidence'")
+        # if (class(zeta)!="incidence")
+        if (!is(zeta, "incidence"))
+          stop("zeta is not an object of class 'incidence'")
     } else zeta <- getzeta(prof)
     ds <- apply(prof$profiles, 1, mpi$rho_k)
     gen.downset(zeta, ds)
